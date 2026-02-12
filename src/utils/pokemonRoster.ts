27e7260cae371716cfec48420d114995c205b6fd
Victor Hernandez, Jr. <vh4697@gmail.com>
2026-02-12 05:47:29 -0600

    pokemon roster is now an array of ids

--- src/utils/pokemonRoster.ts
+++ src/utils/pokemonRoster.ts
@@ -1,31 +1,37 @@
-export const POKEMON_ROSTER: { id: number, category: string }[] = [
-  { id: 3, category: "Grass/Poison" },      // Venusaur
-  { id: 6, category: "Fire/Flying" },       // Charizard
-  { id: 9, category: "Water" },             // Blastoise
-  { id: 25, category: "Electric" },         // Pikachu
-  { id: 59, category: "Fire" },             // Arcanine
-  { id: 68, category: "Fighting" },         // Machamp
-  { id: 94, category: "Ghost/Poison" },     // Gengar
-  { id: 131, category: "Water/Ice" },       // Lapras
-  { id: 143, category: "Normal" },          // Snorlax
-  { id: 149, category: "Dragon" },          // Dragonite
-  { id: 150, category: "Psychic" },         // Mewtwo
-  { id: 151, category: "Psychic" },         // Mew
-  { id: 212, category: "Bug/Steel" },       // Scizor
-  { id: 282, category: "Psychic/Fairy" },   // Gardevoir
-  { id: 398, category: "Normal/Flying" },   // Staraptor
-  { id: 445, category: "Dragon/Ground" },   // Garchomp
-  { id: 448, category: "Fighting/Steel" },  // Lucario
-  { id: 491, category: "Dark" },            // Darkrai
-  { id: 658, category: "Water/Dark" },      // Greninja
-  { id: 663, category: "Fire/Flying" },     // Talonflame
-  { id: 700, category: "Fairy" },           // Sylveon
-  { id: 724, category: "Grass/Ghost" },     // Decidueye
-  { id: 887, category: "Dragon/Ghost" },    // Dragapult
-  { id: 937, category: "Fire/Ghost" },      // Ceruledge
+export const POKEMON_ROSTER = [
+  3,    // Venusaur
+  6,    // Charizard
+  9,    // Blastoise
+  25,   // Pikachu
+  34,		// Nidoking
+  59,   // Arcanine
+  68,   // Machamp
+  94,   // Gengar
+  131,  // Lapras
+  143,  // Snorlax
+  149,  // Dragonite
+  150,  // Mewtwo
+  151,  // Mew
+  212,  // Scizor
+  282,  // Gardevoir
+  398,  // Staraptor
+  445,  // Garchomp
+  448,  // Lucario
+  491,  // Darkrai
+  658,  // Greninja
+  663,  // Talonflame
+  700,  // Sylveon
+  724,  // Decidueye
+  887,  // Dragapult
+  937,  // Ceruledge
+  134,  // Vaporeon   -> Water Absorb
+  135,  // Jolteon    -> Volt Absorb
+  136,  // Flareon    -> Flash Fire 
+  454,  // Toxicroak  -> Dry Skin
+  26,   // Raichu     -> Lightning Rod
+  466,  // Electivire -> Motor Drive
+  423,  // Gastrodon  -> Storm Drain
+  706,  // Goodra     -> Sap Sipper
+  927,  // Dachsbun   -> Well-Baked Body
+  635,  // Hydreigon  -> Levitate
 ];
-
-export const getPokemonCategory = (id: number): string => {
-	const foundPokemon = POKEMON_ROSTER.find(pokemon => pokemon.id === id);
-  return foundPokemon?.category ?? "Unknown";
-};
